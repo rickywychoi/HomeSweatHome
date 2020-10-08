1bd30bdcb522e4f57a10c727e9905ca37f096093
Max Jesus <maxjeseuss@gmail.com>
2020-10-07 20:08:16 -0700

    Strange push containing the first two layouts.

--- app/src/main/java/com/example/homesweathome/MainActivity.java
+++ app/src/main/java/com/example/homesweathome/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.homesweathome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.setBackgroundColor(Color.GRAY);
+    }
+
+    public void SwitchToWorkoutList(View view) {
+        Intent intent = new Intent(this, WorkoutList.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/homesweathome/WorkoutList.java
@@ -0,0 +1,14 @@
+package com.example.homesweathome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_list);
+    }
+}
