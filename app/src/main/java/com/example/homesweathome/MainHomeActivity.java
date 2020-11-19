4842066661e28680c5065204996de87e68e8d71a
rickywychoi <cchaff0105@gmail.com>
2020-11-19 02:59:26 -0800

    Removed firebase test button in mainHome

--- app/src/main/java/com/example/homesweathome/MainHomeActivity.java
+++ app/src/main/java/com/example/homesweathome/MainHomeActivity.java
@@ -29,10 +29,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Date;
 
 public class MainHomeActivity extends AppCompatActivity {
-    // Button and DatabaseReference for firebase connection test
-    Button firebaseTestBtn;
     DatabaseReference database;
-    //
+
     // [START declare_auth]
     private FirebaseAuth mAuth;
     // [END declare_auth]
@@ -44,36 +42,6 @@ public class MainHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_home);
         constraintLayout = findViewById(R.id.constraintLayout);
         constraintLayout.setBackgroundColor(Color.GRAY);
-
-        //
-        // Test if firebase is properly connected
-        //
-        database = FirebaseDatabase.getInstance().getReference("test");
-        firebaseTestBtn = (Button) findViewById(R.id.firebaseBtn);
-        firebaseTestBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String id = database.push().getKey();
-                Task setValueTask = database.child(id).setValue("This is Test - " + new Date());
-                setValueTask.addOnSuccessListener(new OnSuccessListener() {
-                    @Override
-                    public void onSuccess(Object o) {
-                        Toast.makeText(MainHomeActivity.this,
-                                "Successfully connected to firebase", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                setValueTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainHomeActivity.this,
-                                "Something went wrong...", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-        //
-        // end of firebase test
-        //
 
         // [START initialize_auth]
         // Initialize Firebase Auth
@@ -115,15 +83,6 @@ public class MainHomeActivity extends AppCompatActivity {
         mAuth.signOut();
         Toast.makeText(MainHomeActivity.this, "Successfully signed out", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(MainHomeActivity.this, login_page.class));
-//        mGoogleSignInClient.signOut()
-//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<Void> task) {
-//                        Toast.makeText(MainHomeActivity.this,"Successfully signed out",Toast.LENGTH_SHORT).show();
-//                        startActivity(new Intent(MainHomeActivity.this, login_page.class));
-//                        finish();
-//                    }
-//                });
     }
 
     public void switchToWorkoutList(View view) {
